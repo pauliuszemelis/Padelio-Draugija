57d2b0c88a5e5fbd6b1d4bb900654b5c661dce24
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-12-29 16:38:50 +0200

    Initial commit

---
+++ app/view/tpl/login.tpl
@@ -0,0 +1,8 @@
+<div style="text-align: center">
+    <form method="POST" action="?view=users&action=auth">
+        <div>Email:</div><input type="text" name="email"><br>
+        <div>Slaptažodis:</div><input type="password" name="password"><br>
+        <br><br>
+        <input type="submit" class="btn btn-success" value="Prisijungti"><br>
+    </form><br>
+</div>
---
+++ include.php
@@ -0,0 +1,18 @@
+<?php
+
+include('app\Association.php');
+include ('header.php');
+include ('app\controller\TemplateEngineController.php');
+include ('app\controller\ProductController.php');
+include('app\controller\MatchController.php');
+include ('app\controller\UsersController.php');
+
+
+include ('app\model\interfaces\Manageable.php');
+include ('app\model\interfaces\Destroyable.php');
+include ('app\model\CoreModel.php');
+include ('app\model\Product.php');
+include('app\model\MatchHistory.php');
+include ('app\model\Users.php');
+
+define ("SALT", "Labas vakaras brangioji");
