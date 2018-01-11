76c45ab014f09af1686042b7a8e48b61bd0961e4
Paulius Žemelis <paulius.zemelis@gmail.com>
2018-01-11 21:10:17 +0200

    Pradedam butbucketa

---
+++ app/model/MatchPlan.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\model;
+
+use app\model\interfaces\Destroyable;
+use app\model\interfaces\Manageable;
+
+class MatchHistory extends CoreModel implements Manageable, Destroyable {
+
+    protected $table = 'match_history';
+
+    public function create(array $data) {
+
+        $query = $this->generateInsertQuery($data, true);
+        print_r($this->query($query));
+    }
+
+    public function destroy() {
+        
+    }
+
+    public function matchHistory() {
+        $query = "SELECT `Nr`, `Data`, `teammate1`, `teammate2`, `team1_result1`, `team2_result1`, `team1_result2`, `team2_result2`, `team1_result3`, `team2_result3`, `oponent1`, `oponent2` FROM `match_history` WHERE `deleted_at` IS NULL ORDER BY `match_history`.`Data` DESC";
+        return $this->query($query);
+    }
+
+    public function matchPlayers($id) {
+        $query = "SELECT `teammate1`, `teammate2`, `oponent1`, `oponent2` FROM `match_history` WHERE `id`= '$id'";
+        return $this->query($query);
+    }
+    public function update ($id) {
+        $_POST['updeated_by'] = $_COOKIE['user'];
+        $data = $_POST;
+        $options = '';
+        foreach ($data as $key => $value) {
+            $options .= "`$key` = '$value', ";
+        }
+        $options = rtrim($options, ", ");
+        $query = "UPDATE `" . $this->table . "` SET " . $options . " WHERE `id`='$id'";
+        return $this->query($query);
+    }
+
+}
