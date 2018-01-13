ac5597c674844eb7e35cc749a47666c0ce9e40a7
Paulius Žemelis <paulius.zemelis@gmail.com>
2018-01-13 17:32:14 +0200

    STarting planing

---
+++ scripts/datepicker.js
@@ -0,0 +1,8 @@
+
+    
+   
+    <script type='text/javascript'>
+    $( function() {
+        $("#datepicker").datepicker({ dateFormat: 'yy-mm-dd', minDate: 0, maxDate: "+14D" }).val()});
+    </script>
+
