9db1a883b7bf5c38d4a59332de9a13a1ac674ea6
Paulius Žemelis <paulius.zemelis@gmail.com>
2018-01-19 21:58:17 +0200

    dizainas mėlynas

--- index.php
+++ index.php
@@ -4,36 +4,40 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="css\bootstrap.min.css">
         <link rel="icon" type="image/ico" href="uploads\ball.ico">
-        <img id='header' role="banner" src='uploads/padelbanner1.jpg' alt='Banner Image'/>
 
-        <title>Padelio teniso klubas</title>
-        <link rel="stylesheet" href="css/jquery-ui.css">
-        <script src="js/jquery.datepicker.js"></script>
-        <script src="js/jquery-ui.js"></script>
-        <script>
-        $( function() {
-            $( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd', minDate: 0, maxDate: "+14D" }).val();
+    <div class="parent">
+        <img id='header' role="banner" src='uploads/padelblue.jpg' alt='Banner Image'/>
+        <a href="http://padelioklubas.lt" title="Padelio teniso klubas"><img id="pavadinimas" src='uploads/picturetopeople9.png'/></a>
+    </div>
+    <title>Padelio teniso klubas</title>
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <script src="js/jquery.datepicker.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script>
+        $(function () {
+            $("#datepicker").datepicker({dateFormat: 'yy-mm-dd', minDate: 0, maxDate: "+14D"}).val();
         });
-        $( function() {
-        $( "#datepicker2" ).datepicker({ dateFormat: 'yy-mm-dd', minDate: "-14D", maxDate: 0 }).val();
+        $(function () {
+            $("#datepicker2").datepicker({dateFormat: 'yy-mm-dd', minDate: "-14D", maxDate: 0}).val();
         });
-            </script>
-    </head>
-    <body class="body">
-        
-        <?php 
-            include (__DIR__.'/header.php');
-            ?>
-        <div>
-            <?php
+    </script>
+
+</head>
+<body class="body">
+
+    <?php
+    include (__DIR__ . '/header.php');
+    ?>
+    <div>
+        <?php
         include_once ('include.php');
 
         use app\Club;
 
-        $app = new Club();
+$app = new Club();
         ?>
-        </div>
+    </div>
 
-    </body>
-    <footer class="footer">  © 2018 Padelio Teniso Klubas </footer>
+</body>
+<footer class="footer">  © 2018 Padelio Teniso Klubas </footer>
 </html>
